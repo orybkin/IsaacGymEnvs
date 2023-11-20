cc57db0f902cf3bf23d159617f835310c64a41ac
Oleg Rybkin <olehrybkin@gmail.com>
2023-11-19 16:30:00 -0800

    Easy reaching env

--- isaacgymenvs/tasks/franka_reaching2.py
+++ isaacgymenvs/tasks/franka_reaching2.py
@@ -43,6 +43,7 @@ from isaacgymenvs.tasks.franka_pushing import FrankaPushing
 class FrankaReaching2(FrankaPushing):
 
     def __init__(self, *args, **kwargs):
+        self.pushing_like = kwargs['cfg']['env'].get('pushing_like', True)
         super().__init__(*args, **kwargs)
         self.target_idx = []
         self.target_idx = [0, 1, 2]
@@ -60,4 +61,45 @@ class FrankaReaching2(FrankaPushing):
             dist_reward = 1 - torch.tanh(self.dist_reward_dropoff * d)
 
         return self.dist_reward_scale * dist_reward
+
+    
+    def _reset_goal_state(self, env_ids):
+        """
+
+        Args:
+            env_ids (tensor or None): Specific environments to reset cube for
+        """
+        # If env_ids is None, we reset all the envs
+        # TODO randomize Z
+        if env_ids is None:
+            env_ids = torch.arange(start=0, end=self.num_envs, device=self.device, dtype=torch.long)
+
+        # Initialize buffer to hold sampled values
+        num_resets = len(env_ids)
+        sampled_goal_state = torch.zeros(num_resets, 13, device=self.device)
+
+        if self.pushing_like:
+            # Sampling is "centered" around middle of table
+            centered_goal_xyz_state = torch.tensor(self._table_surface_pos[:3], device=self.device, dtype=torch.float32)
+            # Set z value, which is fixed height
+            centered_goal_xyz_state[2] = centered_goal_xyz_state[2] + 0.05
+        else:
+            # Initial arm position
+            centered_goal_xyz_state = torch.tensor([0, 0, 1.21], device=self.device, dtype=torch.float32)
+
+        # Initialize rotation, which is no rotation (quat w = 1)
+        sampled_goal_state[:, 6] = 1.0
+
+        sampled_goal_state[:, :2] = centered_goal_xyz_state[:2].unsqueeze(0) + \
+                                            2.0 * self.goal_position_noise * (
+                                                    torch.rand(num_resets, 2, device=self.device) - 0.5)
+        sampled_goal_state[:, 2] = centered_goal_xyz_state[2]
+
+        if self.test:
+            sampled_goal_state[:, 0] = centered_goal_xyz_state[0] + 0.08
+            sampled_goal_state[:, 1] = centered_goal_xyz_state[1] - 0.08
+            sampled_goal_state[:, 2] = centered_goal_xyz_state[2] 
+
+        # Lastly, set these sampled values as the new init state
+        self._goal_state[env_ids, :] = sampled_goal_state
 
