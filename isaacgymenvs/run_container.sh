697e4526e78dd04d664eda671913b191befc584b
Preston Fu <prestonfu@berkeley.edu>
2024-04-04 15:37:54 -0700

    undo brc updates

--- isaacgymenvs/run_container.sh
+++ isaacgymenvs/run_container.sh
@@ -1,7 +1,10 @@
 echo "$@"
-conda activate cubes2
-export LD_LIBRARY_PATH=/global/scratch/users/prestonfu/miniconda3/envs/cubes2/lib/:$LD_LIBRARY_PATH
-cd /global/scratch/users/prestonfu/taskmaster/isaacgymenvs 
+. /home/miniconda3/etc/profile.d/conda.sh
+conda activate cubes
+export LD_LIBRARY_PATH=/home/miniconda3/envs/cubes/lib/:$LD_LIBRARY_PATH
+cd /global/scratch/users/oleh/taskmaster
+pip install -e . 
+cd isaacgymenvs
 DEVICE=$CUDA_VISIBLE_DEVICES
 unset CUDA_VISIBLE_DEVICES
 python train.py wandb_activate=True sim_device=cuda:$DEVICE rl_device=cuda:$DEVICE graphics_device_id=$DEVICE "$@"
