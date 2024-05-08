389aa955412bbf3a170a993f1ae64013a58ba6eb
Preston Fu <prestonfu@berkeley.edu>
2024-05-08 02:12:35 -0700

    fixes

--- isaacgymenvs/debug_env.py
+++ isaacgymenvs/debug_env.py
@@ -9,6 +9,7 @@ from isaacgym import gymapi
 from isaacgym import gymtorch
 import torch
 import numpy as np
+from copy import deepcopy
 
 
 gym = gymapi.acquire_gym()
@@ -32,6 +33,13 @@ sim = gym.create_sim(0, 0, gymapi.SIM_PHYSX, sim_params)
 if sim is None:
     print("*** Failed to create sim")
     quit()
+    
+def refresh():
+    gym.refresh_actor_root_state_tensor(sim)
+    gym.refresh_dof_state_tensor(sim)
+    gym.refresh_rigid_body_state_tensor(sim)
+    gym.refresh_jacobian_tensors(sim)
+    gym.refresh_mass_matrix_tensors(sim)
 
 # Add ground plane
 plane_params = gymapi.PlaneParams()
@@ -67,8 +75,8 @@ cube_id = gym.create_actor(env, cube_asset, cube_start_pose, "cube", 0, 0, 0)
 
 cube_mass = None
 
-# prepare tensor access
 gym.prepare_sim(sim)
+refresh()
 
 actor_root_state_tensor = gym.acquire_actor_root_state_tensor(sim)
 rigid_body_state_tensor = gym.acquire_rigid_body_state_tensor(sim)
@@ -76,30 +84,37 @@ root_state = gymtorch.wrap_tensor(actor_root_state_tensor).view(1, -1, 13)
 rigid_body_state = gymtorch.wrap_tensor(rigid_body_state_tensor).view(1, -1, 13)
 images = []
 
+n_extra_steps = 0
+sample_goals_flag = False
+n_samples = 5
+
 def reset():
-    states = []
-    for _ in range(1):
-        loc = torch.rand(1, 3, device=device)
-        root_state[0, :, :2] = rigid_body_state[0, :, :2] = 2 * (loc[:, :2] - 0.5)
-        root_state[0, :, 2] = rigid_body_state[0, :, 2] = 1.5 # force z coordinate to be large
+    if sample_goals_flag:
+        states = []
+        for _ in range(n_samples):
+            state = reset_helper()
+            print('candidate:', state)
+            states.append(deepcopy(state))
+        sampled_state = states[np.random.randint(len(states))]
+        print('sampled state:', sampled_state, '\n')
+        root_state[0, :, :3] = sampled_state
         gym.set_actor_root_state_tensor(sim, gymtorch.unwrap_tensor(root_state))
-        for _ in range(10):
+        refresh()
+    else:
+        reset_helper()
+
+
+def reset_helper():
+    loc = torch.rand(1, 3, device=device)
+    root_state[:, :, :2] = 2 * (loc[:, :2] - 0.5)
+    root_state[:, :, 2] = 1.5  # force z coordinate to be large
+    root_state[:, :, 7:10] = 0 # velocity
+    gym.set_actor_root_state_tensor(sim, gymtorch.unwrap_tensor(root_state))
+    if n_extra_steps > 0:
+        for _ in range(n_extra_steps):
             step()
         refresh()
-        states.append(root_state[0, :, :3])
-        print('candidates:', root_state[0, :, :3])
-    
-    sampled_state = states[np.random.randint(len(states))]
-    root_state[0, :, :3] = sampled_state
-    gym.set_actor_root_state_tensor(sim, gymtorch.unwrap_tensor(root_state))    
-    
-
-def refresh():
-    gym.refresh_actor_root_state_tensor(sim)
-    gym.refresh_dof_state_tensor(sim)
-    gym.refresh_rigid_body_state_tensor(sim)
-    gym.refresh_jacobian_tensors(sim)
-    gym.refresh_mass_matrix_tensors(sim)
+    return root_state[0, :, :3]
     
 def step():
     gym.simulate(sim)
@@ -136,18 +151,11 @@ def change_mass(factor):
     if cube_mass is None: cube_mass = properties.mass
     properties.mass = cube_mass * factor
     gym.set_actor_rigid_body_properties(env, cube_id, [properties], True)
-    
-def update_pos(x, y, z):
-    root_state[0, :, :2] = [x, y, z]
-    gym.set_actor_root_state_tensor(sim, gymtorch.unwrap_tensor(root_state))    
 
-run_steps(5)
+for _ in range(3):
+    print('----')
+    reset()
+    run_steps(5)
 
-reset()
-run_steps(5)
-
-# reset()
-# run_steps(5)
-    
 kwargs = {'macro_block_size': None, 'ffmpeg_params': ['-s','1600x912'] }
 imageio.mimsave("render.mp4", images, format='MP4', **kwargs)
