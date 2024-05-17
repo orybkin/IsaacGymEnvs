normalization fixes

isaacgymenvs/clean_awr/awr.py
@@ -141,6 +141,7 @@ class AWRAgent():
             self.rnd_network.to(self.device)
             self.game_extrinsic_rewards = torch_ext.AverageMeter(1, self.games_to_track).to(self.device)
             self.game_shaped_extrinsic_rewards = torch_ext.AverageMeter(1, self.games_to_track).to(self.device)
+            self.game_intrinsic_rewards = torch_ext.AverageMeter(1, self.games_to_track).to(self.device)
 
     def write_stats(self, total_time, epoch_num, step_time, play_time, update_time, metrics, last_lr, lr_mul, frame, scaled_time, scaled_play_time, curr_frames, phase=''):
         # do we need scaled time?
@@ -514,7 +515,8 @@ class AWRAgent():
             for m in range(1, self.config['rnd']['warmup'] + 1):
                 actions = self.uniform_action()
                 obs, _, _, _ = self.env_step(actions)
-                self.rnd_network.update_rms(obs['obs'])
+                self.rnd_network.update_rms_obs(obs['obs'])
+                self.rnd_network.update_rms_rewards(obs['obs'])
                 self.frame += self.config['num_actors']
             self.obs = self.env_reset()
         self.start_frame = self.frame
@@ -551,7 +553,8 @@ class AWRAgent():
                     shaped_rewards = rewards
                     if self.config['rnd']['enable']:
                         extrinsic_rewards = torch.clone(rewards)
-                        intrinsic_rewards = self.rnd_network.update_rms(self.obs['obs']).unsqueeze(1)
+                        self.rnd_network.update_rms_obs(self.obs['obs'])
+                        intrinsic_rewards = self.rnd_network.update_rms_rewards(self.obs['obs']).unsqueeze(1)
                         rewards += self.config['rnd']['coef'] * intrinsic_rewards
 
                     if self.config['value_bootstrap'] and 'time_outs' in infos:
@@ -583,9 +586,11 @@ class AWRAgent():
 
                         self.current_extrinsic_rewards += extrinsic_rewards
                         self.current_shaped_extrinsic_rewards += shaped_extrinsic_rewards
+                        self.current_intrinsic_rewards += intrinsic_rewards
                 
                         self.game_extrinsic_rewards.update(self.current_extrinsic_rewards[env_done_indices])
                         self.game_shaped_extrinsic_rewards.update(self.current_shaped_extrinsic_rewards[env_done_indices])
+                        self.game_intrinsic_rewards.update(self.current_intrinsic_rewards[env_done_indices])
 
                         self.current_extrinsic_rewards = self.current_extrinsic_rewards * not_dones.unsqueeze(1)
                         self.current_shaped_extrinsic_rewards = self.current_shaped_extrinsic_rewards * not_dones.unsqueeze(1)
@@ -713,9 +718,11 @@ class AWRAgent():
                 else:
                     mean_extrinsic_rewards = self.game_extrinsic_rewards.get_mean()
                     mean_shaped_extrinsic_rewards = self.game_shaped_extrinsic_rewards.get_mean()
+                    mean_intrinsic_rewards = self.game_intrinsic_rewards.get_mean()
                     self.writer.add_scalar('extrinsic_rewards', mean_extrinsic_rewards[0], frame)
                     self.writer.add_scalar('shaped_extrinsic_rewards', mean_shaped_extrinsic_rewards[0], frame)
-                    print(f'rewards: {mean_rewards[0]:.3f} ext_rewards: {mean_extrinsic_rewards[0]:.3f} fps total: {fps_total:.0f} epoch: {epoch_num:.0f}{ep_str}')
+                    self.writer.add_scalar('intrinsic_rewards', mean_intrinsic_rewards[0], frame)
+                    print(f'rewards: {mean_rewards[0]:.3f} ext_rewards: {mean_extrinsic_rewards[0]:.3f} int_rewards: {mean_intrinsic_rewards[0]:.3f} fps total: {fps_total:.0f} epoch: {epoch_num:.0f}{ep_str}')
 
             update_time = 0
             

isaacgymenvs/clean_awr/awr_networks.py
@@ -118,18 +118,33 @@ class RNDNetwork(nn.Module):
         self.model = _build_sequential_mlp(self.obs_shape[0], self.config['units'])
         self.target = _build_sequential_mlp(self.obs_shape[0], self.config['units'])
         self.optimizer = optim.Adam(self.model.parameters(), self.config['lr'])
-        self.rms = RunningMeanStd((1,))
+        self.rms_obs = RunningMeanStd(self.obs_shape)
+        self.rms_rewards = RunningMeanStd((1,))
     
-    def mean(self):
-        return self.rms.running_mean.item()
+    def mean(self, running):
+        return running.running_mean
     
-    def std(self):
-        return self.rms.running_var.item() ** 0.5
+    def std(self, running):
+        return running.running_var ** 0.5
 
-    def normalize(self, loss):
-        reward = (loss - self.mean()) / self.std()
-        reward = torch.clip(reward, -5, 5)
-        return reward
+    def normalize_obs(self, obs_batch):
+        out = (obs_batch - self.mean(self.rms_obs)) / self.std(self.rms_obs)
+        out = torch.clip(out, -5., 5.)
+        return out.float()
+
+    def normalize_rewards(self, loss):
+        return (loss / self.std(self.rms_rewards)).float()
+    
+    def update_rms_obs(self, obs_batch):
+        self.rms_obs(obs_batch)
+        return self.normalize_obs(obs_batch)
+    
+    def update_rms_rewards(self, obs_batch):
+        with torch.no_grad():
+            self.model.eval()
+            loss = self.loss(obs_batch)
+            self.rms_rewards(loss)
+        return self.normalize_rewards(loss)
     
     def train(self, obs):
         losses = []
@@ -141,16 +156,10 @@ class RNDNetwork(nn.Module):
             loss.backward()
             self.optimizer.step()
         return torch.tensor(losses).mean()
-    
-    def update_rms(self, obs_batch):
-        with torch.no_grad():
-            self.model.eval()
-            loss = self.loss(obs_batch)
-            self.rms(loss)
-        return self.normalize(loss)
         
     def loss(self, obs_batch):
         """L2 loss per example"""
+        obs_batch = self.normalize_obs(obs_batch)
         model_out = self.model(obs_batch)
         target_out = self.target(obs_batch).detach()
         loss = (model_out - target_out) ** 2