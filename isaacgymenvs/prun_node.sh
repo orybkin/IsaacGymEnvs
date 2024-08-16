441462631fc86a4ec1235b86358b4c78b9087a1e
Oleg Rybkin <olehrybkin@gmail.com>
2024-08-15 17:15:19 -0700

    ppo bug

--- isaacgymenvs/prun_node.sh
+++ isaacgymenvs/prun_node.sh
@@ -3,4 +3,4 @@
 # Run PPO on compute node
 echo $SLURM_JOB_ID
 echo slurm job id
-singularity run --nv --writable-tmpfs  --bind /global/scratch/users/oleh/tmp:/tmp /global/scratch/users/oleh/taskmaster4.sif -- bash /global/scratch/users/oleh/taskmaster/isaacgymenvs/run_container.sh  --slurm_job_id=$SLURM_JOB_ID "$@"
+singularity run --nv --writable-tmpfs  --bind /global/scratch/users/oleh/tmp:/tmp /global/scratch/users/oleh/taskmaster4.sif -- bash /global/scratch/users/oleh/taskmaster/isaacgymenvs/prun_container.sh  --slurm_job_id=$SLURM_JOB_ID "$@"
