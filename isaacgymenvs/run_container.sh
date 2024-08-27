b4c36a7d3c8e49731d3f0ebcc22042fb75357141
Preston Fu <prestonfu@berkeley.edu>
2024-08-27 16:41:42 -0700

    brc update

--- isaacgymenvs/run_container.sh
+++ isaacgymenvs/run_container.sh
@@ -1,9 +1,9 @@
 echo "$@"
-. /home/miniconda3/etc/profile.d/conda.sh
-conda activate cubes2
-export LD_LIBRARY_PATH=/global/scratch/users/prestonfu/miniconda3/envs/cubes2/lib/:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/global/scratch/users/prestonfu/.mujoco/mujoco210/bin
-cd /global/scratch/users/prestonfu/taskmaster/isaacgymenvs 
+. /global/scratch/users/prestonfu/miniconda3/etc/profile.d/conda.sh
+conda activate cubes
+export LD_LIBRARY_PATH=/global/scratch/users/prestonfu/miniconda3/envs/cubes/lib/:$LD_LIBRARY_PATH
+cd /global/scratch/users/prestonfu/taskmaster/isaacgymenvs
 DEVICE=$CUDA_VISIBLE_DEVICES
 unset CUDA_VISIBLE_DEVICES
-python clean_awr/awr.py device=cuda:$DEVICE graphics_device_id=$DEVICE "$@"
+# python train.py wandb_activate=True sim_device=cuda:$DEVICE rl_device=cuda:$DEVICE graphics_device_id=$DEVICE "$@"
+python clean_awr/awr.py "$@"  --agent.device=cuda:$DEVICE --agent.graphics_device_id=$DEVICE
--- isaacgymenvs/run_node.sh
+++ isaacgymenvs/run_node.sh
@@ -2,4 +2,4 @@
 
 echo $SLURM_JOB_ID
 echo slurm job id
-singularity run -B /var/lib/dcv-gl --nv --writable-tmpfs  --bind /global/scratch/users/prestonfu/tmp:/tmp --bind /global/scratch/users/prestonfu/miniconda3/:/home/miniconda3 /global/scratch/users/prestonfu/taskmaster.sif -- bash /global/scratch/users/prestonfu/taskmaster/isaacgymenvs/run_container.sh  slurm_job_id=$SLURM_JOB_ID "$@"
+singularity run --nv --writable-tmpfs  --bind /global/scratch/users/prestonfu/tmp:/tmp /global/scratch/users/prestonfu/taskmaster.sif -- bash /global/scratch/users/prestonfu/taskmaster/isaacgymenvs/run_container.sh  --slurm_job_id=$SLURM_JOB_ID "$@"
